e0a79d042967c57807ed3bec8a92008e2f113738
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-18 16:13:01 -0500

    Added useref to navigate to the sections when clicking the navigation menu

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const about = useRef(null);
+  const experience = useRef(null);
+  const projects = useRef(null);
+  const skills = useRef(null);
+  const awards = useRef(null);
+  const education = useRef(null);
+  const contact = useRef(null);
+  const scrollToSection = (elementRef) =>{
+    window.scroll({
+        top:elementRef.current.offsetTop,
+        behaviour: "smooth"
+    });
+}
   return (
     <div className="App">
-      <div className='hero'>
+      <div className="hero">
         <ul>
-          <li className='link'>About</li>
-          <li className='link'>Experience</li>
-          <li className='link'>Projects</li>
-          <li className='link'>Skills</li>
-          <li className='link'>Awards</li>
-          <li className='link'>Education</li>
-          <li className='link'>Contact</li>
+          <li className="link" onClick={()=>scrollToSection(about)}>About</li>
+          <li className="link" onClick={()=>scrollToSection(experience)}>Experience</li>
+          <li className="link" onClick={()=>scrollToSection(projects)}>Projects</li>
+          <li className="link" onClick={()=>scrollToSection(skills)}>Skills</li>
+          <li className="link" onClick={()=>scrollToSection(awards)}>Awards</li>
+          <li className="link" onClick={()=>scrollToSection(education)}>Education</li>
+          <li className="link" onClick={()=>scrollToSection(contact)}>Contact</li>
         </ul>
       </div>
-      <div className='about'>
-      About
-      </div>
-      <div className='experience'>
-      Experience
-      </div>
-      <div className='projects'>
-      Projects
-      </div>
-      <div className='skills'>
-      Skills
-      </div>
-      <div className='awards'>
-      Awards
-      </div>
-      <div className='education'>
-      Education
-      </div>
-      <div className='contact'>
-      Contact
-      </div>
+      <div className="about" ref={about} >About</div>
+      <div className="experience" ref={experience}>Experience</div>
+      <div className="projects" ref={projects}>Projects</div>
+      <div className="skills" ref={skills}>Skills</div>
+      <div className="awards" ref={awards}>Awards</div>
+      <div className="education" ref={education}>Education</div>
+      <div className="contact" ref={contact}>Contact</div>
     </div>
   );
 }

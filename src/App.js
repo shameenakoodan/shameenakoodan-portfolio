54caeca36190ae9e35694fd48684362867cf9705
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-18 15:33:03 -0500

    Added the hyperlinks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Skills</li>
+          <li>Awards</li>
+          <li>Education</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
   );
 }

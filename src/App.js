fba0aa0b2d3f0479368e67a6c2c3b0edbd12ba84
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-18 15:52:08 -0500

    Added the contents for the menu

--- src/App.js
+++ src/App.js
@@ -4,16 +4,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className='hero'>
         <ul>
-          <li>About</li>
-          <li>Experience</li>
-          <li>Projects</li>
-          <li>Skills</li>
-          <li>Awards</li>
-          <li>Education</li>
-          <li>Contact</li>
+          <li className='link'>About</li>
+          <li className='link'>Experience</li>
+          <li className='link'>Projects</li>
+          <li className='link'>Skills</li>
+          <li className='link'>Awards</li>
+          <li className='link'>Education</li>
+          <li className='link'>Contact</li>
         </ul>
+      </div>
+      <div className='about'>
+      About
+      </div>
+      <div className='experience'>
+      Experience
+      </div>
+      <div className='projects'>
+      Projects
+      </div>
+      <div className='skills'>
+      Skills
+      </div>
+      <div className='awards'>
+      Awards
+      </div>
+      <div className='education'>
+      Education
+      </div>
+      <div className='contact'>
+      Contact
       </div>
     </div>
   );
